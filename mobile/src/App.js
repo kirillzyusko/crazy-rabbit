95167f1320b18155bb395bf7cefa1db34877c7ba
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-03-07 21:43:09 +0300

    Mobile: added keyframes animation

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -4,57 +4,94 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Animated,
+    Image,
+    Easing
 } from 'react-native';
 import SvgObject from './components/svg-object.native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
+    ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
+    android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-          <SvgObject />
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    constructor () {
+        super()
+        this.animatedValue = new Animated.Value(0)
+    }
+    componentDidMount () {
+        this.animate()
+    }
+    animate () {
+        this.animatedValue.setValue(0)
+        Animated.timing(
+            this.animatedValue,
+            {
+                toValue: 1,
+                duration: 2000,
+                easing: Easing.linear()
+            }
+        ).start(() => this.animate())
+    }
+    render () {
+        const marginLeft = this.animatedValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 300]
+        })
+        const opacity = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 1, 0]
+        })
+        const movingMargin = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 300, 0]
+        })
+        const textSize = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [18, 32, 18]
+        })
+        const rotateX = this.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['0deg', '180deg', '0deg']
+        })
+        return (
+            <View style={styles.container}>
+                <Animated.View
+                    style={{
+                        marginLeft: movingMargin,
+                        marginTop: 10}}>
+                    <SvgObject/>
+                </Animated.View>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
--- mobile/src/components/svg-object.native.js
+++ mobile/src/components/svg-object.native.js
@@ -2,32 +2,36 @@ import React, { Component } from 'react';
 import Svg,{
     Circle,
     Rect,
+    G,
+    Path
 } from 'react-native-svg';
 
 class SvgObject extends Component {
     render() {
         return (
-            <Svg
-                height="100"
-                width="100"
-            >
-                <Circle
-                    cx="50"
-                    cy="50"
-                    r="45"
-                    stroke="blue"
-                    strokeWidth="2.5"
-                    fill="green"
-                />
-                <Rect
-                    x="15"
-                    y="15"
-                    width="70"
-                    height="70"
-                    stroke="red"
-                    strokeWidth="2"
-                    fill="yellow"
-                />
+            <Svg height={300} width={300}>
+            <G>
+                <Path d="m1.25,1.25h256v256h-256z" fill="#cd9945" stroke="#6e441b" stroke-width="2.5"/>
+                <Path d="m33.25,33.25h192v192h-192z" fill="#9a6926"/>
+                <Path d="m-225.25,33.25h6v192h-6z" fill="#976825" stroke="#6e441b" stroke-width="2.5" transform="matrix(0,-1,1,0,0,0)"/>
+                <Path d="m-136.4201,203.8853h271.53v6h-271.53z" fill="#6e441b" opacity=".75" transform="matrix(.70711,-.70711,.70711,.70711,0,0)"/>
+                <Path d="m33.4823,71.65h191.2738" fill="none" stroke="#6e441b" stroke-width="2.5"/>
+                <Path d="m33.25,110.05h191.2738" fill="none" stroke="#70461c" stroke-width="2.5"/>
+                <Path d="m33.25,148.45h191.2738" fill="none" stroke="#6e441b" stroke-width="2.5"/>
+                <Path d="m33.25,186.85h191.2738" fill="none" stroke="#6e441b" stroke-width="2.5"/>
+                <Path d="m18.2938,47.0229h6v271.53h-6z" fill="#976825" stroke="#6e441b" stroke-width="2.5" transform="matrix(.70711,-.70711,.70711,.70711,0,0)"/>
+                <Path d="m32,34h192v6h-192z" fill="#6e441b" opacity=".75"/>
+                <Path d="m33.25,33.25h6v192h-6z" fill="#976825" stroke="#70461c" stroke-width="2.5"/>
+                <Path d="m45.7324,22.9842h271.53v6h-271.53z" fill="#6e441b" opacity=".75" transform="rotate(45)"/>
+                <Path d="m40.5,34.5h6v192h-6z" fill="#6f451b" opacity=".75"/>
+                <Path d="m-21.7784,47.0221h40v271.53h-40z" fill="#cd9945" stroke="#6e441b" stroke-width="2.5" transform="matrix(.70711,-.70711,.70711,.70711,0,0)"/>
+                <Path d="m162.7871-134.6081h40v273h-40z" fill="#cd9945" stroke="#6e441b" stroke-width="2.5" transform="rotate(45)"/>
+                <Path d="m11.9691,3.2876h240.1544v29.3436h-240.1544z" fill="#cd9945"/>
+                <Path d="m14.9158,225.6797h240.1544v29.3436h-240.1544z" fill="#cd9945"/>
+                <Path d="m6.7625-32.4324h240.1544v29.3436h-240.1544z" fill="#cd9945" transform="matrix(0,1,-1,0,0,0)"/>
+                <Path d="m12.4013-254.6841h240.1544v29.3436h-240.1544z" fill="#cd9945" transform="matrix(0,1,-1,0,0,0)"/>
+                <Path d="m33.25,33.25h192v192h-192z" fill="none" stroke="#6e441b" stroke-width="2.5"/>
+            </G>
             </Svg>
         );
     }
