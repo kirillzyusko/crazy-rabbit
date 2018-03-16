dabdfada37e4b269b519eca82876f16997339848
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-03-16 14:06:25 +0300

    Correct displaying at xhdpi screen

--- mobile/src/engine/constants.js
+++ mobile/src/engine/constants.js
@@ -4,9 +4,9 @@ export const { width, height } = Dimensions.get('window');
 //alignment
 const defaultBlockHeight = 258.5;
 export const blockScalability = 0.25;
-export const distanceWithRespectToGround = width / 4.63;
 export const heightOfOneBlock = defaultBlockHeight * blockScalability;
 export const groundHeight = height * 0.75;
+export const distanceWithRespectToGround = groundHeight / 2.4;
 
 //characteristics of hero
 export const heightOfJump = heightOfOneBlock * 1.7;
