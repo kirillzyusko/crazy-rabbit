95226aef87128da50cb5f44ab7c98b805e79d1d8
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-03-21 15:41:24 +0300

    Selected hero save at redux

--- src/containers/gallery.native.js
+++ src/containers/gallery.native.js
@@ -3,9 +3,10 @@ import {
     View,
     TouchableOpacity
 } from 'react-native';
-import PropTypes from 'prop-types';
-import Arrow from './../components/menu/arrow.native';
 import Svg, {G} from 'react-native-svg';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Arrow from './../components/menu/arrow.native';
 import Hero from "../components/models/heroes/hero.native";
 import {
     height, heightOfArrow,
@@ -16,6 +17,7 @@ import {
 import { listOfHero } from "../engine/constants/hero";
 import { getHeroByType } from "../utils/hero.native";
 import BackgroundForest from '../components/models/environment/ambient/background/background-forest.native';
+import {CHOOSE_HERO} from "../actions/index";
 
 const style = {
   position: 'absolute'
@@ -32,13 +34,13 @@ const styleRightArrow = {
     top: (height)/2,
     left: (width-widthOfHero)/2
 };
-
+//todo: make as common component
 class Gallery extends PureComponent {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            index: 0
+            index: listOfHero.findIndex(type => props.hero.type === type)
         }
     }
 
@@ -56,6 +58,7 @@ class Gallery extends PureComponent {
                 index + 1;
 
         this.setState({index: newIndex});
+        this.props.setHeroType(listOfHero[newIndex]);
     };
 
     onClickBack = () => this.onClickHandler(true);
@@ -91,4 +94,15 @@ class Gallery extends PureComponent {
     }
 }
 
-export default Gallery;
+const mapDispatchToProps = dispatch => ({
+    setHeroType: (heroType) => {
+        dispatch({type: CHOOSE_HERO, payload: heroType})
+    }
+});
+
+const mapStateToProps = state => ({
+    hero: state.hero
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gallery);
