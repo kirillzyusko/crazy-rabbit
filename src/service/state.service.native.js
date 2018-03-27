9ccbbef6ce227e9491665e31562689f53aaba178
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-03-27 23:22:40 +0300

    Added initial state for first run of application

--- src/service/state.service.native.js
+++ src/service/state.service.native.js
@@ -1,4 +1,5 @@
 import { AsyncStorage } from 'react-native';
+import { isUndefined } from 'lodash';
 
 const KEY = 'STATE';
 const TIMEOUT = 400;
@@ -6,19 +7,36 @@ const TIMEOUT = 400;
 class GameService {
 	static timeout = () =>  new Promise(resolve => setTimeout(resolve, TIMEOUT));
 
+	static init = async () => {
+		const initialState = {levels: [0]}; // first level with 0 star
+		await GameService.saveUserData(initialState);
+		return await GameService.getUserData();
+	};
+
 	static getUserData = async () => {
 		try {
 			await GameService.timeout();
 			const value = await AsyncStorage.getItem(KEY);
-			if (value !== null) {
-				return JSON.parse(value);
+
+			if (value === null || isUndefined(value)) {
+				return GameService.init();
 			}
+
+			return JSON.parse(value);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-	static saveUserData = async (newData) => {
+	static saveUserData = async (data) => {
+		try {
+			return await AsyncStorage.setItem(KEY, JSON.stringify(data));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	static updateUserData = async (newData) => {
 		try {
 			const oldData = await GameService.getUserData();
 			const data = {
